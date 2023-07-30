c9a6267d762119dc0c423bc8791e9e828609a86b
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2023-07-30 13:36:58 +0300

    Created basic window

--- src/Panel.java
+++ src/Panel.java
@@ -1,14 +1,38 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.util.Random;
 
 public class Panel extends JPanel implements ActionListener {
-    Panel() {
+    static final int SCREEN_WIDTH = 500;
+    static final int SCREEN_HEIGHT = 500;
+    static final int UNIT_SIZE = 20;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 50;
+    final  int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
+    Panel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.decode("#073b4c"));
+        this.setFocusable(true);
+        this.addKeyListener(new GameKeyAdapter());
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
+
+    private class GameKeyAdapter extends KeyAdapter {}
 }
