a4ccfc2a472b4d28e4a651524302b1e67068ea9b
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2023-07-30 13:20:28 +0300

    Project setup

---
+++ src/Frame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Frame extends JFrame {
+    Frame() {
+        this.add(new Panel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
---
+++ src/Panel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Panel extends JPanel implements ActionListener {
+    Panel() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
